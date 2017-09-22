14aa37175d62f9474ef5ffce4463810474b4a694
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-22 14:36:12 -0400

    Updated from Brython Server: 9/22/2017 2:36:11 PM

--- house.py
+++ house.py
@@ -11,7 +11,7 @@ blue = Color(0x0000FF,1)
 
 blackOutline = LineStyle(1,black) #pixels, color
 redRectangle = RectangleAsset(200,200,blackOutline,red) #width, height, outline, fill
-redTriangle = PolygonAsset([(0,0), (200,0), (100,-150)],blackOutline,red) #list of endpoints
+redTriangle = PolygonAsset([(0,0), (200,0), (100,-100)],blackOutline,red) #list of endpoints
 text = TextAsset('Mi Casa',fill=green,style='bold 40pt Times')
 
 
