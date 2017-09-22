ea103bab575bd93835cc06ec5214ff72c7ba0f32
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-22 14:46:02 -0400

    Updated from Brython Server: 9/22/2017 2:46:01 PM

--- germany.py
+++ germany.py
@@ -1,1 +1,24 @@
+#Sam Smedinghoff
+#9/20/17
+#house.py - Makes a house
+
+from ggame import *
+
+red = Color(0xFF0000,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+
+
+blackOutline = LineStyle(1,black) #pixels, color
+redRectangle = RectangleAsset(200,200,blackOutline,red) #width, height, outline, fill
+redRectangle = RectangleAsset(200,200,blackOutline,red) #width, height, outline, fill
+text = TextAsset('Flag of Germany',fill=black,style='bold 40pt Times')
+
+
+Sprite(redRectangle,(300,200)) #(right,down))
+Sprite(yellowRectangle,(300,200))
+Sprite(blackRectangle,(300,200))
+Sprite(text,(100, 100))
+App().run()
+
 
