698840687cea41918b28f7663dedfe7cc5939754
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-22 14:35:59 -0400

    Updated from Brython Server: 9/22/2017 2:35:58 PM

--- house.py
+++ house.py
@@ -6,15 +6,18 @@ from ggame import *
 
 red = Color(0xFF0000,1)
 black = Color(0x000000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+
 blackOutline = LineStyle(1,black) #pixels, color
 redRectangle = RectangleAsset(200,200,blackOutline,red) #width, height, outline, fill
-redTriangle = PolygonAsset([(0,0), (120,180), (60,300)],blackOutline,red) #list of endpoints
-text = TextAsset('Smeds',fill=green,style='bold 40pt Times')
+redTriangle = PolygonAsset([(0,0), (200,0), (100,-150)],blackOutline,red) #list of endpoints
+text = TextAsset('Mi Casa',fill=green,style='bold 40pt Times')
 
 
 Sprite(redRectangle,(300,200)) #(right,down))
 Sprite(redTriangle,(300,200))
-Sprite(text,(200, 100))
+Sprite(text,(100, 100))
 App().run()
 
 
