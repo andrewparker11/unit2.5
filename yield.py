aea431f59dc0fe2ac9daaf0aa8548c53f1d8a14e
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-26 15:47:05 -0400

    Updated from Brython Server: 9/26/2017 3:47:04 PM

--- yield.py
+++ yield.py
@@ -10,13 +10,11 @@ black = Color(0x000000,1)
 blackOutline = LineStyle(1,black) #pixels, color
 
 
-blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, linestyle
 whiteTriangle = PolygonAsset([(0,0), (300,00), (150,250)],blackOutline,white) #list of endpoints
 redTriangle = PolygonAsset([(0,0), (300,0), (150,250)],blackOutline,red) #list of endpoints
 text = TextAsset('YIELD',fill=red,style='bold 40pt Times')
 
 Sprite(whiteTriangle,(200,100))
-Sprite(blackLine)
 Sprite(redTriangle,(200,100))
 Sprite(text,(300,200))
 App().run()
