9faffc886ac0a78e07e6c9d29de9152366449ccd
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-26 14:17:40 -0400

    Updated from Brython Server: 9/26/2017 2:17:40 PM

--- warmup5.py
+++ warmup5.py
@@ -9,10 +9,10 @@ blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 blackOutline = LineStyle(1,black) #pixels, color
 
-yellowDiamond = PolygonAsset([(0,0), (120,180), (60,300)],blackOutline,yellow) #list of endpoints
-text = TextAsset('Andrew Parker',fill=blue,style='bold 40pt Times')
+yellowDiamond = PolygonAsset([(0,0), (120,180), (60,300),(180,120)],blackOutline,yellow) #list of endpoints
+text = TextAsset('Andrew Parker',fill=blue,style='bold 20pt Times')
 
 
-Sprite(yellowDiamond,(50,50)) #(right,down)
+Sprite(yellowDiamond,(150,150)) #(right,down)
 Sprite(text,(300,200))
 App().run()
