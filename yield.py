b8a12811b88d398944b5c88855882959056bf0d8
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-26 14:54:34 -0400

    Updated from Brython Server: 9/26/2017 2:54:34 PM

--- yield.py
+++ yield.py
@@ -9,16 +9,13 @@ white = Color(0x000000,1)
 black = Color(0x000000,1)
 blackOutline = LineStyle(1,black) #pixels, color
 
-redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
-blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, film)
-greenEllipse = EllipseAsset(100,50,blackOutline,green) #horizontal radius, vertical_radius, outline, fill
+
 blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, linestyle
+whiteTriangle = PolygonAsset([(0,0), (120,180), (60,300)],blackOutline,white) #list of endpoints
 redTriangle = PolygonAsset([(0,0), (120,180), (60,300)],blackOutline,red) #list of endpoints
 text = TextAsset('Smeds',fill=green,style='bold 40pt Times')
 
-Sprite(redRectangle)
-Sprite(blueCircle,(50,50)) #(right,down)
-Sprite(greenEllipse,(200,400)) #(right,down)
+Sprite(whiteTriangle)
 Sprite(blackLine)
 Sprite(redTriangle)
 Sprite(text,(300,200))
