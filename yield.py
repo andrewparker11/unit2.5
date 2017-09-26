9d5a7c2d5029e759c80ebe677104562af21312af
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-26 14:52:21 -0400

    Updated from Brython Server: 9/26/2017 2:52:20 PM

--- yield.py
+++ yield.py
@@ -5,8 +5,7 @@
 from ggame import *
 
 red = Color(0xFF0000,1)
-green = Color(0x00FF00,1)
-blue = Color(0x0000FF,1)
+white = Color(0x000000,1)
 black = Color(0x000000,1)
 blackOutline = LineStyle(1,black) #pixels, color
 
