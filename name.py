2bb4a6fc8b483df3ad8215e24f469366fc3a0e26
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-26 14:34:30 -0400

    Updated from Brython Server: 9/26/2017 2:34:30 PM

--- name.py
+++ name.py
@@ -2,23 +2,21 @@
 #9/22/17
 #name.py - Gives you a "cool" name banner
 
+name = input('What is your name')
+ccolor = (input('Enter an RGB code' ))
+
+
 from ggame import *
 
-red = Color(0xFF0000,1)
+bcolor = Color(ccolor,1)
 black = Color(0x000000,1)
-yellow = Color(0xFFFF00,1)
 
 
 blackOutline = LineStyle(1,black) #pixels, color
-redRectangle = RectangleAsset(300,50,blackOutline,red) #width, height, outline, fill
-blackRectangle = RectangleAsset(300,50,blackOutline,black)
-yellowRectangle = RectangleAsset(300,50,blackOutline,yellow)
-text = TextAsset('Flag of Germany',fill=black,style='bold 20pt Times')
+bRectangle = RectangleAsset(300,300,blackOutline,bcolor)
+text = TextAsset(name,fill=black,style='bold 20pt Times')
 
-
-Sprite(redRectangle,(300,150)) #(right,down))
-Sprite(yellowRectangle,(300,200))
-Sprite(blackRectangle,(300,100))
+Sprite(bRectangle,(300,100))
 Sprite(text,(400, 25))
 App().run()
 
