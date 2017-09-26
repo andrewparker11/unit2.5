bc8317a26448998c07c7de2d491e202b582a6a38
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-26 14:49:28 -0400

    Updated from Brython Server: 9/26/2017 2:49:28 PM

--- yield.py
+++ yield.py
@@ -1,1 +1,27 @@
+#Sam Smedinghoff
+#9/26/17
+#yield.py - Gives you a yield sign
+
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+blackOutline = LineStyle(1,black) #pixels, color
+
+redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
+blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, film)
+greenEllipse = EllipseAsset(100,50,blackOutline,green) #horizontal radius, vertical_radius, outline, fill
+blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, linestyle
+redTriangle = PolygonAsset([(0,0), (120,180), (60,300)],blackOutline,red) #list of endpoints
+text = TextAsset('Smeds',fill=green,style='bold 40pt Times')
+
+Sprite(redRectangle)
+Sprite(blueCircle,(50,50)) #(right,down)
+Sprite(greenEllipse,(200,400)) #(right,down)
+Sprite(blackLine)
+Sprite(redTriangle)
+Sprite(text,(300,200))
+App().run()
 
